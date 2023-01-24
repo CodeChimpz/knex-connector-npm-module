d6d44ea8390d193f480a63a20136397a55af6949
taburet <md12072004@gmail.com>
2023-01-24 15:59:04 +0200

    pre-test

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import {config} from "dotenv";
+import {WinstonLoggerService} from "../logger";
 
 config()
 import knex, {Knex} from "knex";
+import now from "performance-now";
 
 interface SelectOptions {
-    orderBy?: Array<string | { column: string, order: string }>
     limit?: number
     offset?: number
 }
@@ -16,15 +17,19 @@ class Repository {
     constructor(knex: Knex, schema: string) {
         this.knex = knex
         this.schema = schema
+
     }
 
-    async find(select: Array<string> | string, where: (builder: Knex.QueryBuilder) => Knex.QueryBuilder, options: SelectOptions) {
-        const {limit, offset, orderBy} = options
+    async find(select: Array<string> | string,
+               where: (builder: Knex.QueryBuilder) => Knex.QueryBuilder,
+               orderBy: Array<string | { column: string, order: string }>,
+               options: SelectOptions) {
+        const {limit, offset} = options
         return this.knex(this.schema).select(select)
             .where(where)
+            .orderBy(orderBy)
             .limit(limit || 100)
             .offset(offset || 0)
-            .orderBy(<any>orderBy)
     }
 
     async create(object: any) {
@@ -42,14 +47,47 @@ class Repository {
 
 export class DataSource {
     knex: Knex
+    queriesRan: Map<string, { start?: number, time?: number, overrun?: boolean }>
+    logger: WinstonLoggerService
 
-    constructor(knexClientConfig: Knex.Config) {
+    constructor(knexClientConfig: Knex.Config, logger: WinstonLoggerService) {
+        this.logger = logger
         this.knex = knex(knexClientConfig)
+        this.queriesRan = new Map()
+        this.knex
+            .on('query', (query) => {
+                const time = now()
+                const id = <string>query.__knexQueryUid
+                this.queriesRan.set(id, {
+                    start: time
+                })
+            })
+            .on('query-response', (response, query) => {
+                const time = now()
+                const id = <string>query.__knexQueryUid
+                const start = this.queriesRan.get(id)?.start
+                if (!start) {
+                    throw 'Escaped query start'
+                }
+                this.queriesRan.set(id, {
+                    time: time - start
+                })
+            })
+            .on('query-error', (error, query) => {
+                const id = <string>query.__knexQueryUid
+                if (error.name === 'KnexTimeoutError') {
+                    this.queriesRan.set(id, {
+                        time: query.timeout,
+                        overrun: true
+                    })
+                }
+                this.logger.app.error('Query timeout , id: ' + id, {id, error})
+            })
     }
 
     async createSchema(name: string, schema: any) {
-        if (await this.knex.schema.hasTable(name) && !process.env.DROP_TABLES) {
-            throw new Error('Error creating table with name ' + name + ', it probably already exists')
+        if (await this.knex.schema.hasTable(name)) {
+            return
         }
         await this.knex.schema.createTable(name, (table: any) => {
             Object.getOwnPropertyNames(schema).forEach(property => {
