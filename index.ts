f74a664cb270eb59c3529edbb912909bae6bbe93
taburet <md12072004@gmail.com>
2023-02-13 16:17:43 +0200

    'patch'

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ config()
 import knex, {Knex} from "knex";
 import now from "performance-now";
 
+//QUERY running interfaces
 interface SelectOptions {
     limit?: number
     offset?: number
@@ -17,6 +18,22 @@ interface WhereObjI {
     options?: SelectOptions
 }
 
+export enum SchemaDataTypes {
+    // primary = 'primary',
+    integer = 'integer',
+    string = 'string',
+    datetime = 'datetime'
+}
+
+
+interface SchemaDefinitionT {
+    [key: string]: {
+        type: SchemaDataTypes
+    }
+}
+
+
+//Repo class
 export class Repository {
     knex: Knex
     schema: string
@@ -24,7 +41,6 @@ export class Repository {
     constructor(knex: Knex, schema: string) {
         this.knex = knex
         this.schema = schema
-
     }
 
     async find(opts: WhereObjI) {
@@ -37,7 +53,11 @@ export class Repository {
     }
 
     async create(object: any) {
-        return this.knex(this.schema).insert(object)
+        const res = await this.knex(this.schema).insert(object).then(obj => {
+            return this.knex(this.schema).select().where('_id', obj[0])
+        })
+        return res[0]
+
     }
 
     async edit(data: any, where: (builder: Knex.QueryBuilder) => Knex.QueryBuilder) {
@@ -58,50 +78,68 @@ export class DataSource {
         this.logger = logger
         this.knex = knex(knexClientConfig)
         this.queriesRan = new Map()
-        this.knex
-            .on('query', (query) => {
-                const time = now()
-                const id = <string>query.__knexQueryUid
-                this.queriesRan.set(id, {
-                    start: time
-                })
-            })
-            .on('query-response', (response, query) => {
-                const time = now()
-                const id = <string>query.__knexQueryUid
-                const start = this.queriesRan.get(id)?.start
-                if (!start) {
-                    throw 'Escaped query start'
-                }
-                this.queriesRan.set(id, {
-                    time: time - start
-                })
-            })
-            .on('query-error', (error, query) => {
-                const id = <string>query.__knexQueryUid
-                if (error.name === 'KnexTimeoutError') {
+        try {
+            this.knex.raw('SELECT 1')
+            this.logger.http.info('Connected to database')
+            this.knex
+                .on('query', (query) => {
+                    const time = now()
+                    const id = <string>query.__knexQueryUid
                     this.queriesRan.set(id, {
-                        time: query.timeout,
-                        overrun: true
+                        start: time
                     })
-                }
-                this.logger.app.error('Query timeout , id: ' + id, {id, error})
-            })
-    }
-
-    async createSchema(name: string, schema: any) {
-        if (await this.knex.schema.hasTable(name)) {
-            return
+                })
+                .on('query-response', (response, query) => {
+                    const time = now()
+                    const id = <string>query.__knexQueryUid
+                    const start = this.queriesRan.get(id)?.start
+                    if (!start) {
+                        throw 'Escaped query start'
+                    }
+                    this.queriesRan.set(id, {
+                        time: time - start
+                    })
+                })
+                .on('query-error', (error, query) => {
+                    const id = <string>query.__knexQueryUid
+                    if (error.name === 'KnexTimeoutError') {
+                        this.queriesRan.set(id, {
+                            time: query.timeout,
+                            overrun: true
+                        })
+                    }
+                    this.logger.app.error('Query timeout , id: ' + id, {id, error})
+                })
+        } catch (e: any) {
+            this.logger.app.error(e)
         }
-        await this.knex.schema.createTable(name, (table: any) => {
-            Object.getOwnPropertyNames(schema).forEach(property => {
-                table[schema[property].type](property)
-            })
-        })
 
     }
 
-    async getRepo(name: string) {
+    //Creates a table in db from defined Schema, returns a class to use for initialisation of Models
+    async createSchema(name: string, schema: SchemaDefinitionT) {
+        const check = await this.knex.schema.hasTable(name)
+        if (!check) {
+            //run createTable query with all the defined types
+            await this.knex.schema.createTable(name, (table: any) => {
+                table.increments('_id').primary()
+                Object.entries(schema).forEach(property => {
+                    const [name, meta] = property
+                    table[meta.type](name)
+                    // switch (meta.type) {
+                    //     case SchemaDataTypes.primary:
+                    //         table.increments(name).primary()
+                    //         break;
+                    //     default:
+                    //         table[meta.type](name)
+                    //         break;
+                    // }
+                })
+            })
+            //
+        }
+        this.logger.app.info('Created table "' + name + '"')
         return new Repository(this.knex, name)
     }
+
 }
